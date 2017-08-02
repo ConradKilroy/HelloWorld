4a26259ccb0439b32cf601d71586df7ca57bb81d
ConradKilroy <conradkilroy@gmail.com>
2017-08-02 06:15:03 -0700

    Create Setup.R

---
+++ Setup.R
@@ -0,0 +1,19 @@
+my_libraries <- c("ggmap","sp","rgdal","rgeos", "ggplot2","leaflet","dplyr", "magrittr",
+                  "readr", "lubridate", "RColorBrewer", "classInt")
+
+install.packages(my_libraries)
+
+
+## These are some of my favorite packages for spatial data analysis
+suppressPackageStartupMessages(library(ggmap))
+suppressPackageStartupMessages(library(sp))
+suppressPackageStartupMessages(library(rgdal))
+suppressPackageStartupMessages(library(rgeos))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(leaflet))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(RColorBrewer))
+suppressPackageStartupMessages(library(classInt))
